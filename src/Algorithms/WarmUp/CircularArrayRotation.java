91d1c8fb1849fd0a4fdc22ce3ff81a065d3f6847
Benjineer <Benjineer@NIYI-PC>
2017-02-16 11:12:04 +0100

    commit 2

--- src/Algorithms/WarmUp/CircularArrayRotation.java
+++ src/Algorithms/WarmUp/CircularArrayRotation.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package hackerrankcodes;
+package Algorithms.WarmUp;
 
 import java.util.Scanner;
 
@@ -11,24 +11,23 @@ import java.util.Scanner;
  *
  * @author DATA INFOSEC
  */
-public class Staircase {
+public class CircularArrayRotation {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-
         int n = in.nextInt();
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (j >= (n - 1) - i) {
-                    System.out.print("#");
-                } else {
-                    System.out.print(" ");
-                }
-            }
-            System.out.println("");
+        int k = in.nextInt();
+        int q = in.nextInt();
+        int[] a = new int[n];
+        for (int a_i = 0; a_i < n; a_i++) {
+            a[a_i] = in.nextInt();
         }
 
+        for (int a0 = 0; a0 < q; a0++) {
+            int m = in.nextInt();
+        }
+
+        
     }
 
 }
